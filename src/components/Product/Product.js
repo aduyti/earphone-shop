1a2e586136f1d5ca55e5ba001badfa5f555b1599
Anirudhya <anirudhyaduyti@gmail.com>
2022-03-26 04:35:25 +0600

    card btn

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Product.css';
 
 const Product = ({ product, clickHandler }) => {
-    const { image, name, price } = product;
+    const { image, name, price, id } = product;
     return (
         <div className="product-card">
             <img className="card-img" src={image} alt={name} />
@@ -10,6 +10,7 @@ const Product = ({ product, clickHandler }) => {
                 <h3 style={{ marginBottom: '10px' }}>{name}</h3>
                 <h4 style={{ margin: '0', color: 'darkgrey' }}>Price: ${price}</h4>
             </div>
+            <button className="card-btn" onClick={() => clickHandler(id)}>Add to Cart <i className="bi bi-cart-plus"></i></button>
         </div>
     );
 };
